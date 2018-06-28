0dadfecc1e5ce5d2a2032d628aeb610716eaf922
Roman Boykov <gendalf90@outlook.com>
2018-06-28 21:52:07 +0300

    changing the name of interface methods

--- AggregateException.Extensions.Handler/HandlerBuilder.cs
+++ AggregateException.Extensions.Handler/HandlerBuilder.cs
@@ -17,21 +17,21 @@ namespace AggregateExceptionExtensions.Handler
             exception.Handle(chainHandler.TryHandle);
         }
 
-        public IHandlerBuilder Handler<T>() where T : Exception
+        public IHandlerBuilder Ignore<T>() where T : Exception
         {
             var handler = new TypedHandler<T>();
             chainHandler.AddHandler(handler);
             return this;
         }
 
-        public IHandlerBuilder Handler<T>(Action<T> action) where T : Exception
+        public IHandlerBuilder Action<T>(Action<T> action) where T : Exception
         {
             var handler = new TypedHandlerWithAction<T>(action);
             chainHandler.AddHandler(handler);
             return this;
         }
 
-        public IHandlerBuilder Handler<T>(Func<T, bool> predicate) where T : Exception
+        public IHandlerBuilder Predicate<T>(Func<T, bool> predicate) where T : Exception
         {
             var handler = new TypedHandlerWithPredicate<T>(predicate);
             chainHandler.AddHandler(handler);
--- AggregateException.Extensions.Handler/IHandlerBuilder.cs
+++ AggregateException.Extensions.Handler/IHandlerBuilder.cs
@@ -4,9 +4,11 @@ namespace AggregateExceptionExtensions.Handler
 {
     public interface IHandlerBuilder
     {
-        IHandlerBuilder Handler<T>(Action<T> action) where T : Exception;
+        IHandlerBuilder Ignore<T>() where T : Exception;
 
-        IHandlerBuilder Handler<T>(Func<T, bool> predicate) where T : Exception;
+        IHandlerBuilder Action<T>(Action<T> action) where T : Exception;
+
+        IHandlerBuilder Predicate<T>(Func<T, bool> predicate) where T : Exception;
 
         void Handle();
     }
